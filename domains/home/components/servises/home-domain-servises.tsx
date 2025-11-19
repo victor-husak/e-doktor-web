a68cff0716747f8c8c7ac9d36748ebad410179b2
Victor Husak <lascari565758@gmail.com>
2025-11-20 00:49:44 +0200

    fix

--- domains/home/components/servises/home-domain-servises.tsx
+++ domains/home/components/servises/home-domain-servises.tsx
@@ -37,10 +37,15 @@ export const HomeDomainServises: React.FC<HomeDomainServisesProps> = (
           description="Track your doses, access verified digital certificates, receive reminders for upcoming shots, and instantly share your medical records with clinics or schools — all in one tap."
           ActionComponent={
             <>
-              <Button title="Book a demo" IconComponent={<VideoIcon />} />
+              <Button
+                animation="scale"
+                title="Book a demo"
+                IconComponent={<VideoIcon />}
+              />
 
               <Button
                 variant="second"
+                animation="scale"
                 title="Download app"
                 IconComponent={<DownloadIcon color="#1D1D1F" />}
               />
