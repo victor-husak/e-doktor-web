d11f5c445238d3acd48d3d0842adea7bf2e25b3b
Victor Husak <lascari565758@gmail.com>
2026-05-27 23:18:42 +0300

    fix

--- domains/home/servises/ServisesFooter.tsx
+++ domains/home/servises/ServisesFooter.tsx
@@ -25,9 +25,8 @@ export const ServisesFooter: React.FC<ServisesFooterProps> = (
       )}
     >
       <p>
-        {t("help")}
-        {/* {" "}
-        <span className="link-dashed text-[#2C2C2C]">{t("help.1")}</span> */}
+        {t("help.0")}{" "}
+        <span className="link-dashed text-[#2C2C2C]">{t("help.1")}</span>
       </p>
 
       <p>
